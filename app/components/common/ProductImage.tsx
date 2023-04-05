283a28326acd5d674ff7a8c325ee03be906f42b5
cspencer <cspencernd@gmail.com>
2023-04-05 05:36:22 -0400

    init common components

---
+++ app/components/common/ProductImage.tsx
@@ -0,0 +1,40 @@
+import type { Image as ImageT } from "@shopify/hydrogen-react/storefront-api-types"
+import { Image } from "@shopify/hydrogen-react"
+import cn from "clsx"
+
+// const nullImage = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1024 1024'%3E%3C/svg%3E`
+
+type ProductImageProps = {
+    image: ImageT
+    title: string
+    rounded?: true | false | "top" | "bottom"
+    containerProps?: React.HTMLAttributes<HTMLDivElement>
+    imageProps?: React.HTMLAttributes<HTMLImageElement>
+}
+
+export default function ProductImage(props: ProductImageProps) {
+    const { image, title, rounded = true, containerProps, imageProps } = props
+    const altText = image?.altText ?? title
+
+    return (
+        <figure
+            className={cn(
+                "bg-glass overflow-hidden",
+                rounded === true ? "rounded-2xl" : "",
+                rounded === "top" ? "rounded-t-2xl" : "",
+                rounded === "bottom" ? "rounded-b-2xl" : "",
+                containerProps?.className ?? ""
+            )}
+        >
+            <Image
+                data={image}
+                role="presentation"
+                alt={altText ?? "loading"}
+                // src={image?.url ?? nullImage}
+                // width={image?.width ?? 1024}
+                // height={image?.height ?? 1024}
+                {...imageProps}
+            />
+        </figure>
+    )
+}
