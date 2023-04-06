16238f7e29d20072dd7b771d7f1b285cc5266778
cspencer <cspencernd@gmail.com>
2023-04-06 04:16:15 -0400

    chore: move theme to separate file

---
+++ theme.js
@@ -0,0 +1,19 @@
+module.exports = {
+    next: {
+        primary:                "hsl(208, 90%, 15%)",
+        "primary-content":      "hsl(205, 99%, 47%)",
+        secondary:              "hsl(262, 67%, 22%)",
+        "secondary-content":    "hsl(266, 85%, 68%)",
+        accent:                 "hsl(302, 63%, 19%)",
+        "accent-content":       "hsl(300, 58%, 53%)",
+        neutral:                "hsl(225, 12%, 48%)",
+        "base-100":             "hsl(225, 12%, 20%)",
+        "base-200":             "hsl(225, 12%, 12%)",
+        "base-300":             "hsl(225, 12%, 8%)",
+        info:                   "hsl(170, 79%, 43%)",
+        success:                "hsl(145, 70%, 46%)",
+        warning:                "hsl(49, 88%, 49%)",
+        error:                  "hsl(346, 87%, 57%)",
+        "--border-btn":         "0px",
+    },
+}
