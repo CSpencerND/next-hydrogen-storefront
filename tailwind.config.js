5074431994ffa71495eb1719e14f43ffd460fc0f
cspencer <cspencernd@gmail.com>
2023-04-08 03:54:32 -0400

    feat: add shadcn/ui and all it's dependencies and tweak tailwing config accordingly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: ["class", '[data-theme="dark"]'],
     content: ["./app/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             fontSize: {
-                xs:     ["clamp(0.8rem, calc(0.68rem + 0.29vw), 1rem)", "1.2"],
-                sm:     ["clamp(1.00rem, calc(0.92rem + 0.39vw), 1.20rem)", "1.5"],
-                base:   ["clamp(1.13rem, calc(0.98rem + 0.73vw), 1.50rem)", "1.5"],
-                lg:     ["clamp(1.27rem, calc(1.03rem + 1.19vw), 1.88rem)", "1.5"],
-                xl:     ["clamp(1.42rem, calc(1.06rem + 1.80vw), 2.34rem)", "1.4"],
-                "2xl":  ["clamp(1.60rem, calc(1.08rem + 2.59vw), 2.73rem)", "1.2"],
-                "3xl":  ["clamp(1.80rem, calc(1.08rem + 3.63vw), 3.66rem)", "1.1"],
-                "4xl":  ["clamp(2.03rem, calc(1.03rem + 4.98vw), 4.58rem)", "1"],
-                "5xl":  ["clamp(2.28rem, calc(0.94rem + 6.71vw), 5.72rem)", "1"],
-                "6xl":  ["clamp(2.57rem, calc(0.78rem + 8.95vw), 7.15rem)", "1"],
+                xs: ["clamp(0.8rem, calc(0.68rem + 0.29vw), 1rem)", "1.2"],
+                sm: ["clamp(1.00rem, calc(0.92rem + 0.39vw), 1.20rem)", "1.5"],
+                base: ["clamp(1.13rem, calc(0.98rem + 0.73vw), 1.50rem)", "1.5"],
+                lg: ["clamp(1.27rem, calc(1.03rem + 1.19vw), 1.88rem)", "1.5"],
+                xl: ["clamp(1.42rem, calc(1.06rem + 1.80vw), 2.34rem)", "1.4"],
+                "2xl": ["clamp(1.60rem, calc(1.08rem + 2.59vw), 2.73rem)", "1.2"],
+                "3xl": ["clamp(1.80rem, calc(1.08rem + 3.63vw), 3.66rem)", "1.1"],
+                "4xl": ["clamp(2.03rem, calc(1.03rem + 4.98vw), 4.58rem)", "1"],
+                "5xl": ["clamp(2.28rem, calc(0.94rem + 6.71vw), 5.72rem)", "1"],
+                "6xl": ["clamp(2.57rem, calc(0.78rem + 8.95vw), 7.15rem)", "1"],
             },
             boxShadow: {
                 box: "0 0 10px rgb(0 0 0 / 0.2), 0 0 5px rgb(0 0 0 / 0.2)",
@@ -22,6 +23,20 @@ module.exports = {
                 height: "height",
                 spacing: "margin, padding",
             },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
         },
     },
 
@@ -29,6 +44,7 @@ module.exports = {
         require("daisyui"),
         require("@tailwindcss/typography"),
         require("@headlessui/tailwindcss")({ prefix: "ui" }),
+        require("tailwindcss-animate"),
     ],
 
     daisyui: {
