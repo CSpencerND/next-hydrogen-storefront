9a350438f123059cdb18d7d8df6c1227bbb5c9eb
cspencer <cspencernd@gmail.com>
2023-04-10 10:37:11 -0400

    feat: add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingSpinner } from "./components/common"
+
+export default function LoadingPage() {
+    return (
+        <section className="min-h-screen">
+            <LoadingSpinner />
+        </section>
+    )
+}
