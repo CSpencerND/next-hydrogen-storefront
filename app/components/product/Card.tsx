c1575ffbe0184083cd032a0cb642e012b78384b5
cspencer <cspencernd@gmail.com>
2023-04-11 13:30:48 -0400

    refactor: cleanup to stay dry

--- app/components/product/Card.tsx
+++ app/components/product/Card.tsx
@@ -1,20 +1,32 @@
 import Link from "next/link"
 
-import type { ElementType, PropsWithChildren } from "react"
+import type { PropsWithChildren } from "react"
 
 type CardProps = PropsWithChildren<{
     href?: string
 }>
 
 export function Card({ href, children, ...props }: CardProps) {
-    if (href) {
+    return (
+        <li className="card h-full overflow-hidden rounded-2xl ">
+            <CardBody
+                href={href}
+                {...props}
+            >
+                {children}
+            </CardBody>
+        </li>
+    )
+}
+
+function CardBody({ href, children, ...props }: CardProps) {
+    if (!!href) {
         return (
             <Link
                 href={href}
                 className={`
-                    card relative h-full overflow-hidden
-                    rounded-2xl text-primary-content
-                    transition-all hover:brightness-105 active:scale-95
+                    relative text-primary-content transition
+                    hover:brightness-105 active:scale-95
                 `}
                 {...props}
             >
@@ -22,43 +34,13 @@ export function Card({ href, children, ...props }: CardProps) {
             </Link>
         )
     }
+
     return (
         <div
-            className="card relative h-full overflow-hidden rounded-2xl transition-all"
+            className="relative"
             {...props}
         >
             {children}
         </div>
     )
 }
-
-// type WrapperProps<P = Record<string, unknown>> = PropsWithChildren & {
-//     as?: ElementType
-// } & P
-
-// type CardProps<P = Record<string, unknown>> = PropsWithChildren & {
-//     href?: string
-//     props?: P
-// }
-
-// function Wrapper({ children, as: Component = Link, ...props }: WrapperProps) {
-//     return <Component {...props}>{children}</Component>
-// }
-
-// export function Card({ href, children, ...props }: CardProps) {
-//     return (
-//         <Wrapper
-//             as={href ? Link : "div"}
-//             href={href}
-//             className={`
-//                 card relative h-full overflow-hidden rounded-2xl
-//                 text-primary-content transition-all
-//                 hover:scale-105 hover:brightness-105
-//                 active:scale-95
-//             `}
-//             {...props}
-//         >
-//             {children}
-//         </Wrapper>
-//     )
-// }
