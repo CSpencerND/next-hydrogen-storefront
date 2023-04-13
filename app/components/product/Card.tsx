3583fbb5a6b275cd32768d0b3514a5e234722ef3
cspencer <cspencernd@gmail.com>
2023-04-13 14:28:31 -0400

    perf: simplify

--- app/components/product/Card.tsx
+++ app/components/product/Card.tsx
@@ -1,38 +1,26 @@
+import { cn } from "@/lib/utils"
+
 import Link from "next/link"
 
 import type { PropsWithChildren } from "react"
-import { Swatch } from "./Swatch"
 
 type CardProps = PropsWithChildren<{
     href?: string
-    colorSwatch?: boolean
+    rounded?: boolean
 }>
 
-export function Card({ href, children, colorSwatch, ...props }: CardProps) {
-    return (
-        <li className="card h-full overflow-hidden">
-            <CardBody
-                href={href}
-                {...props}
-            >
-                {children}
-            </CardBody>
-            {colorSwatch && (
-                <Swatch className="overflow-x-scroll bg-gradient-to-t from-base-200 to-base-100 p-4" />
-            )}
-        </li>
-    )
-}
-
-function CardBody({ href, children, ...props }: CardProps) {
+export function Card({ href, children, rounded, ...props }: CardProps) {
     if (!!href) {
         return (
             <Link
                 href={href}
-                className={`
-                    relative text-primary-content transition
+                className={cn(`
+                    relative h-full overflow-hidden
+                    text-primary-content transition
                     hover:brightness-105 active:scale-95
-                `}
+                `,
+                    rounded ? "rounded-2xl" : ""
+                )}
                 {...props}
             >
                 {children}
@@ -42,7 +30,7 @@ function CardBody({ href, children, ...props }: CardProps) {
 
     return (
         <div
-            className="relative"
+            className="relative h-full overflow-hidden"
             {...props}
         >
             {children}
