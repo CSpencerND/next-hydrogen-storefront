dd2d3bc733a0c2fc34abd3371b849e560ea17c3d
cspencer <cspencernd@gmail.com>
2023-04-14 13:17:47 -0400

    feat: add url

--- app/components/product/Item.tsx
+++ app/components/product/Item.tsx
@@ -10,23 +10,25 @@ import { Title } from "./Title"
 /** Renders a full product card with all it's subcomponents.
  *  Must be wrapped in a ProductProvider.
  */
-export function Item() {
+export function Item({collection}: {collection: string}) {
     const currentImage = useProduct((s) => s.currentImage)
-    const title = useProduct((s) => s.product.title)
-    const handle = useProduct((s) => s.product.handle)
+    const title = useProduct((s) => s.product.title) as string
+    const handle = useProduct((s) => s.product.handle) as string
+
+    const PRODUCT_URL = `/collections/${collection}/${handle}` as const
 
     return (
         <li className="card relative h-full overflow-hidden rounded-2xl">
-            <Card href="\0">
+            <Card href={PRODUCT_URL}>
                 <ProductImage
                     image={currentImage}
-                    title={title ?? "Product"}
+                    title={title}
                     rounded={false}
                 />
                 <Title
                     overlayed
                     truncate
-                    title={title ?? "Product"}
+                    title={title}
                 />
             </Card>
             <Swatch attached />
