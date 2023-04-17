89dd55ca83bb5d9d0608628497f8f9f0a916cfd4
cspencer <cspencernd@gmail.com>
2023-04-17 14:34:25 -0400

    style: negative margin to decrease space relative to title

--- app/components/product/Price.tsx
+++ app/components/product/Price.tsx
@@ -1,15 +1,19 @@
 "use client"
 
+import { cn } from "@/lib/utils"
+
 import { useProductStore } from "@/lib/state"
 import { ProductPrice } from "@shopify/hydrogen-react"
 
-export function Price() {
+import type { ClassName } from "types"
+
+export function Price({ className }: ClassName) {
     const product = useProductStore((s) => s.product)
 
     return (
         <ProductPrice
             as="p"
-            className="text-xs md:text-sm"
+            className={cn("-mt-6 text-xs md:text-sm", className)}
             data={product}
             priceType="regular"
             valueType="max"
