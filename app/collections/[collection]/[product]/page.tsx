592fbcf9bbbd53dfeda701708d2383462eda3d59
cspencer <cspencernd@gmail.com>
2023-04-27 03:46:13 -0400

    perf: generate static url for each product

--- app/collections/[collection]/[product]/page.tsx
+++ app/collections/[collection]/[product]/page.tsx
@@ -1,9 +1,17 @@
-import { getProductByHandle } from "@/lib/storefront"
+import { getFeaturedCollection, getProductByHandle } from "@/lib/storefront"
 
 import { Breadcrumbs } from "@/components/navigation"
 import Product from "@/components/product"
 
 import type { FullPathParams } from "../layout"
+
+export async function generateStaticParams({ params }: FullPathParams) {
+    const products = await getFeaturedCollection(params.collection)
+
+    return products.map((p) => ({
+        product: p.handle,
+    }))
+}
 
 export default async function ProductDynamicSegment({ params }: FullPathParams) {
     const p = await getProductByHandle(params.product)
