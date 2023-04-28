b2cac06b823deff4b2ff95c87b4e27e1512e0bdf
cspencer <cspencernd@gmail.com>
2023-04-28 03:03:15 -0400

    fix: adjust breadcrumbs to nav correctly; style: remove bg;

--- app/components/navigation/Breadcrumbs.tsx
+++ app/components/navigation/Breadcrumbs.tsx
@@ -9,19 +9,20 @@ export function Breadcrumbs() {
 
     return (
         <section className="breadcrumbs mx-auto text-sm lg:max-w-7xl">
-            <ul className="bg-blur-200 w-fit rounded-2xl p-4 shadow-lg [&_a]:text-primary-content">
-                {segments.map((segment, i) => {
-                    const path = `${segments.slice(0, i + 1).join("/")}`
+            {/* <ul className="bg-blur-200 w-fit rounded-2xl p-4 shadow-lg [&_a]:text-primary-content"> */}
+            <ul className="w-fit p-4 [&_a]:text-primary-content">
+                {segments.map((s, i) => {
+                    const path = `/${segments.slice(0, i + 1).join("/")}`
                     if (i !== segments.length - 1) {
                         return (
-                            <li key={segment}>
-                                <Link href={path}>{segment}</Link>
+                            <li key={s}>
+                                <Link href={path}>{s}</Link>
                             </li>
                         )
                     }
                     return (
-                        <li key={segment}>
-                            <span className="text-accent-content">{segment}</span>
+                        <li key={s}>
+                            <span className="text-accent-content">{s}</span>
                         </li>
                     )
                 })}
