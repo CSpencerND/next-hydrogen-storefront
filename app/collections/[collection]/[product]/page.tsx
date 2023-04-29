416cebf5734d83018dc91f1300174e9ee75072b7
cspencer <cspencernd@gmail.com>
2023-04-29 01:45:12 -0400

    perf: separate recommended into its own file

--- app/collections/[collection]/[product]/page.tsx
+++ app/collections/[collection]/[product]/page.tsx
@@ -1,11 +1,7 @@
-import {
-    getCollectionByHandle,
-    getProductByHandle,
-    getProductRecommendations,
-} from "@/lib/storefront"
+import { getCollectionByHandle, getProductByHandle } from "@/lib/storefront"
 
 import Product from "@/components/product"
-import Collection from "@/components/collection"
+import RecommendedProducts from "./recommended"
 
 import type { FullPathParams } from "../layout"
 
@@ -19,7 +15,6 @@ export async function generateStaticParams({ params }: FullPathParams) {
 
 export default async function ProductDynamicSegment({ params }: FullPathParams) {
     const p = await getProductByHandle(params.product)
-    const r = await getProductRecommendations(p.id)
 
     return (
         <>
@@ -44,29 +39,8 @@ export default async function ProductDynamicSegment({ params }: FullPathParams) 
                     <Product.Description text={p.descriptionHtml} />
                 </Product.Layout.Body>
             </Product.Layout>
-            <section>
-                <h3 className="mb-4 text-lg font-bold text-accent-content">
-                    You may also like
-                </h3>
-                <ul className="mx-auto grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-4">
-                    {r.slice(0, 4).map((p) => {
-                        const { id, title, featuredImage, onlineStoreUrl, handle } = p
-                        return (
-                            <Collection.Card
-                                href={onlineStoreUrl ?? `/collections/full-catalog/${handle}`}
-                                key={id}
-                                rounded
-                            >
-                                <Collection.Image
-                                    title={title}
-                                    image={featuredImage!}
-                                />
-                                <Collection.Title title={title} centered />
-                            </Collection.Card>
-                        )
-                    })}
-                </ul>
-            </section>
+            {/* @ts-expect-error Server Component */}
+            <RecommendedProducts productID={p.id} />
         </>
     )
 }
---
+++ app/collections/[collection]/[product]/recommended.tsx
@@ -0,0 +1,33 @@
+import Collection from "@/components/collection"
+import { getProductRecommendations } from "@/lib/storefront"
+
+export default async function RecommendedProducts({ productID }: { productID: string }) {
+    const r = await getProductRecommendations(productID)
+
+    return (
+        <section>
+            <h3 className="mb-4 text-lg font-bold text-accent-content">You may also like</h3>
+            <ul className="mx-auto grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-4">
+                {r.slice(0, 4).map((p) => {
+                    const { id, title, featuredImage, onlineStoreUrl, handle } = p
+                    return (
+                        <Collection.Card
+                            href={onlineStoreUrl ?? `/collections/full-catalog/${handle}`}
+                            key={id}
+                            rounded
+                        >
+                            <Collection.Image
+                                title={title}
+                                image={featuredImage!}
+                            />
+                            <Collection.Title
+                                title={title}
+                                centered
+                            />
+                        </Collection.Card>
+                    )
+                })}
+            </ul>
+        </section>
+    )
+}
