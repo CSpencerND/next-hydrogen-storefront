2f072a12a0f7025050ec59f0ad739aa8135dd616
cspencer <cspencernd@gmail.com>
2023-05-01 02:23:58 -0400

    style: decrease text size for small screen

--- app/components/collection/Heading.tsx
+++ app/components/collection/Heading.tsx
@@ -13,7 +13,7 @@ export function Heading({ collectionTitle, collectionDescription }: HeadingProps
             initial={{ opacity: 0, x: "-20%" }}
             animate={{ opacity: 1, x: 0 }}
             className="prose mt-4 max-w-prose-narrow prose-headings:mb-4">
-            <h1 className="text-accent-content">{collectionTitle}</h1>
+            <h1 className="text-accent-content max-sm:text-3xl">{collectionTitle}</h1>
 
             {collectionDescription && (
                 <aside
