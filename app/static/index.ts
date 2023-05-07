5867b821afdfbefdb3b44d2197fd567af0fd5646
cspencer <cspencernd@gmail.com>
2023-05-07 18:19:28 -0400

    feat: add static placeholder images

--- app/static/index.ts
+++
@@ -1,3 +0,0 @@
-/** import your static assets here */
-
-export { }
---
+++ app/static/index.tsx
@@ -0,0 +1,6 @@
+import hero from "./hero.webp"
+import product from "./product.webp"
+import logoMain from "./logoMain.webp"
+import logoSquare from "./logoSquare.webp"
+
+export { hero, product, logoMain, logoSquare }
