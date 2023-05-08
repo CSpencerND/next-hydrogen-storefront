6ff5d65e8b62facacbece3afa0f151fa0ac442a4
cspencer <cspencernd@gmail.com>
2023-05-08 16:39:53 -0400

    refactor: hero cleanup

--- app/components/ui/Hero.tsx
+++ app/components/ui/Hero.tsx
@@ -10,44 +10,61 @@ type HeroProps = {
     font?: NextFont
     rounded?: boolean
     buttonText?: string
+    aspect?: "portrait" | "landscape"
 }
 
-export async function Hero({ font, rounded, buttonText = "Go Shopping!", ...props }: HeroProps) {
+export async function Hero({
+    font,
+    rounded = true,
+    aspect = "landscape",
+    buttonText = "Go Shopping!",
+    ...props
+}: HeroProps) {
     const { brand } = await getShop()
-
     return (
-        <section className={cn("hero overflow-hidden shadow-lg", rounded ? "rounded-3xl" : "")}>
-            <Image
-                className="aspect-video max-h-[calc(100vh-196px)] object-cover object-top"
-                src={hero}
-                alt={brand?.slogan ?? brand?.coverImage?.alt ?? ""}
-                placeholder="blur"
-                priority
-                {...props}
-            />
+        <section
+            className="aspect-h-9 aspect-w-16 max-h-[calc(100vh-192px)]"
+            {...props}
+        >
             <div
-                aria-hidden={true}
-                className="hero-overlay bg-black/30"
-            />
-            <div className="hero-content flex flex-col text-center">
-                <h1
-                    className={cn(font?.className ?? "", "text-3xl")}
-                    style={{
-                        textShadow: "1px 2px 2px black, -1px -2px 2px black",
-                    }}
-                >
-                    {brand?.slogan ?? brand?.coverImage?.alt}
-                </h1>
-                <Link
-                    href="/collections"
-                    className="glass btn relative text-primary-content shadow-box shadow-primary-content"
-                >
-                    <div
-                        aria-hidden={true}
-                        className="absolute left-0 top-0 -z-10 h-full w-full rounded-xl bg-primary/60 backdrop-blur-lg backdrop-saturate-150"
-                    />
-                    {buttonText}
-                </Link>
+                className={cn(
+                    "hero overflow-hidden shadow-lg",
+                    aspect === "portrait" ? "min-h-screen" : "",
+                    rounded ? "rounded-3xl" : ""
+                )}
+            >
+                <Image
+                    className="h-auto w-full object-cover object-center"
+                    src={hero}
+                    alt={brand?.slogan ?? brand?.coverImage?.alt ?? ""}
+                    placeholder="blur"
+                    priority
+                />
+                <div
+                    className="hero-overlay"
+                    aria-hidden={true}
+                />
+                <div className="relative grid h-5/6 grid-rows-3 text-center text-neutral-content">
+                    <div aria-hidden={true} />
+                    <h1
+                        className={cn(font?.className ?? "", "text-3xl")}
+                        style={{
+                            textShadow: "1px 2px 2px black, -1px -2px 2px black",
+                        }}
+                    >
+                        {brand?.slogan ?? brand?.coverImage?.alt}
+                    </h1>
+                    <Link
+                        href="/collections"
+                        className="glass btn relative mx-auto w-fit text-primary-content shadow-box shadow-primary-content"
+                    >
+                        <div
+                            className="absolute left-0 top-0 -z-10 h-full w-full rounded-xl bg-primary/60 backdrop-blur-lg backdrop-saturate-150"
+                            aria-hidden={true}
+                        />
+                        {buttonText}
+                    </Link>
+                </div>
             </div>
         </section>
     )
