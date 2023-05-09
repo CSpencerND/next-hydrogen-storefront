468bf75b2fba36283986871cd113afd1c21d2511
cspencer <cspencernd@gmail.com>
2023-05-09 09:01:50 -0400

    refactor: update with new format

--- app/collections/[collection]/[product]/recommended.tsx
+++ app/collections/[collection]/[product]/recommended.tsx
@@ -1,5 +1,6 @@
-import Collection from "@/components/collection"
 import { getProductRecommendations } from "@/lib/storefront"
+import NextImage from "next/image"
+import NextLink from "next/link"
 
 export default async function RecommendedProducts({ productID }: { productID: string }) {
     const r = await getProductRecommendations(productID)
@@ -9,26 +10,34 @@ export default async function RecommendedProducts({ productID }: { productID: st
             <h3 className="mb-4 text-lg font-bold text-info">You may also like</h3>
             <ul className="mx-auto grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-4">
                 {r.slice(0, 4).map((p) => {
-                    const { id, title, featuredImage, onlineStoreUrl, handle } = p
+                    const { id, title, featuredImage: image, handle } = p
+                    const href = `/collections/full-catalog/${handle}`
+
                     return (
-                        <Collection.Card
-                            // href={onlineStoreUrl ?? `/collections/full-catalog/${handle}`}
-                            href={`/collections/full-catalog/${handle}`}
-                            key={id}
-                            rounded
-                        >
-                            <Collection.Image
-                                title={title}
-                                image={featuredImage!}
-                            />
-                            <Collection.Title
-                                className="text-sm sm:text-base md:text-sm lg:text-base"
-                                title={title}
-                                truncated
-                                centered
-                                rounded
-                            />
-                        </Collection.Card>
+                        <li key={id}>
+                            <NextLink
+                                className="card image-full card-compact grid-cols-1 !rounded-3xl before:hidden"
+                                href={href}
+                                key={id}
+                            >
+                                <figure className="glass rounded-3xl">
+                                    {image && (
+                                        <NextImage
+                                            src={image?.url}
+                                            alt={image.altText ?? title ?? " "}
+                                            width={image.width ?? 1024}
+                                            height={image.height ?? 1024}
+                                            role="presentation"
+                                        />
+                                    )}
+                                </figure>
+                                <span className="card-body mt-auto rounded-b-3xl bg-white/10 !py-2 backdrop-blur-[10px] backdrop-saturate-[1.8]">
+                                    <h2 className="truncate text-center text-sm font-bold text-primary-content">
+                                        {title}
+                                    </h2>
+                                </span>
+                            </NextLink>
+                        </li>
                     )
                 })}
             </ul>
--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,7 +1,7 @@
 import Collection from "@/components/collection"
-import { LoadingSpinner } from "@/components/ui"
 import { getCollections } from "@/lib/storefront"
-import { Suspense } from "react"
+import NextLink from "next/link"
+import NextImage from "next/image"
 
 const getData = async () => {
     const collections = await getCollections()
@@ -27,30 +27,30 @@ export default async function CollectionDirectoryPage() {
             <Collection.Grid>
                 {collections.map(({ id, title, href, image }) => {
                     return (
-                        <Collection.Card
-                            href={href}
-                            rounded
-                            key={id}
-                        >
-                            <Suspense fallback={<LoadingSpinner />}>
-                                {image ? (
-                                    <Collection.Image
-                                        image={image}
-                                        title={title}
-                                    />
-                                ) : (
-                                    <p className="flex flex-col text-center text-base font-bold">
-                                        <span>Collection</span>
-                                        <span>Image</span>
-                                    </p>
-                                )}
-                                <Collection.Title
-                                    title={title}
-                                    centered
-                                    rounded
-                                />
-                            </Suspense>
-                        </Collection.Card>
+                        <li key={id}>
+                            <NextLink
+                                className="card image-full card-compact grid-cols-1 !rounded-3xl before:hidden"
+                                href={href}
+                                key={id}
+                            >
+                                <figure className="glass rounded-3xl">
+                                    {image && (
+                                        <NextImage
+                                            src={image?.url}
+                                            alt={image.altText ?? title ?? " "}
+                                            width={image.width ?? 1024}
+                                            height={image.height ?? 1024}
+                                            role="presentation"
+                                        />
+                                    )}
+                                </figure>
+                                <span className="card-body mt-auto rounded-b-3xl bg-white/10 !py-2 backdrop-blur-[10px] backdrop-saturate-[1.8]">
+                                    <h2 className="truncate text-center text-sm font-bold text-primary-content">
+                                        {title}
+                                    </h2>
+                                </span>
+                            </NextLink>
+                        </li>
                     )
                 })}
             </Collection.Grid>
