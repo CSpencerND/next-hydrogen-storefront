42293b3cc10b5fe13a1ddf1a3b25d9cc7cdf10ad
cspencer <cspencernd@gmail.com>
2023-05-11 21:54:14 -0400

    refactor: implement new card component and remove deprecated

--- app/components/product/Card.tsx
+++ app/components/product/Card.tsx
@@ -1,26 +1,20 @@
-import type { Product as TProduct, Image as TImage } from "@shopify/hydrogen-react/storefront-api-types"
-import type { ReactNode } from "react"
+"use client"
 
-type CardProps = {
-    href: URL
-    product?: TProduct
-    image?: TImage
-    children: (image: TProduct["featuredImage"], title: TProduct["title"]) => ReactNode
-}
+import { ProductCard } from "@/components"
 
-export function Card({ children, href, product, image }: CardProps) {
-    const { featuredImage: image, title } = product
+import { useProductStore } from "@/lib/state"
+
+import type { ComponentProps } from "react"
+type CardProps = Omit<ComponentProps<typeof ProductCard>, "image">
+
+export function Card({ href, title }: CardProps) {
+    const currentImage = useProductStore((s) => s.currentImage)
 
     return (
-        <Link
-            className="card image-full card-compact grid-cols-1 !rounded-3xl before:hidden"
-            variants={fadeInUp}
+        <ProductCard
+            image={currentImage}
             href={href}
-        >
-            {children(image, title)}
-            <span className="card-body mt-auto rounded-b-3xl bg-white/10 !py-2 backdrop-blur-[10px] backdrop-saturate-[1.8]">
-                <h2 className="truncate text-center text-sm font-bold text-primary-content">{title}</h2>
-            </span>
-        </Link>
+            title={title}
+        />
     )
 }
--- app/components/product/index.tsx
+++ app/components/product/index.tsx
@@ -1,53 +1,29 @@
 import Children from "types"
 
-// import { Card } from "./Card"
+import { Card } from "./Card"
 import { CartButton } from "./CartButton"
 import { Description } from "./Description"
-import { Image } from "./Image"
 import { Layout } from "./Layout"
 import { Link } from "./Link"
 import { Modal } from "./Modal"
 import { Price } from "./Price"
 import { Size } from "./Size"
-import { Static } from "./Static"
 import { Swatch } from "./Swatch"
 import { Title } from "./Title"
 import { Variant } from "./Variant"
 
-function Product({ children }: Children) {
-    return <>{children}</>
-}
+const Product = ({ children }: Children) => <>{children}</>
 
-// Product.Card = Card
+Product.Card = Card
 Product.CartButton = CartButton
 Product.Description = Description
-Product.Image = Image
 Product.Layout = Layout
 Product.Link = Link
 Product.Modal = Modal
 Product.Price = Price
 Product.Size = Size
-Product.Static = Static
 Product.Swatch = Swatch
 Product.Title = Title
 Product.Variant = Variant
 
 export default Product
-
-// export function StaticProductCard({ href, product, image }: React.ComponentProps<typeof Card>) {
-//     return (
-//         <Card
-//             href={href}
-//             product={product}
-//         >
-//             {(image, title) =>
-//                 image && (
-//                     <Figure
-//                         image={image}
-//                         title={title}
-//                     />
-//                 )
-//             }
-//         </Card>
-//     )
-// }
