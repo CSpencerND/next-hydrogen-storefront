3afda8acdf78fd63aa4f3a8debee72a92574140d
cspencer <cspencernd@gmail.com>
2023-05-11 23:47:39 -0400

    chore: remove redundant fragment

--- app/collections/[collection]/page.tsx
+++ app/collections/[collection]/page.tsx
@@ -11,30 +11,28 @@ export default async function CollectionDynamicSegment({ params }: CollectionSeg
     const { collectionTitle, collectionDescription, products } = data
 
     return (
-        <>
-            <Collection.Section>
-                <Collection.Heading
-                    collectionTitle={collectionTitle}
-                    collectionDescription={collectionDescription}
-                />
-                <Collection.Grid>
-                    {products.map((p) => {
-                        if (!p) throw new Error("No product found!")
+        <Collection.Section>
+            <Collection.Heading
+                collectionTitle={collectionTitle}
+                collectionDescription={collectionDescription}
+            />
+            <Collection.Grid>
+                {products.map((p) => {
+                    if (!p) throw new Error("No product found!")
 
-                        const { title, handle } = p
-                        const href = `/collections/${params.collection}/${handle}`
+                    const { title, handle } = p
+                    const href = `/collections/${params.collection}/${handle}`
 
-                        return (
-                            <li key={p.id}>
-                                <ProductProvider product={p}>
-                                    <Product.Card {...{ title, href }} />
-                                    {/* <Product.Swatch attached /> */}
-                                </ProductProvider>
-                            </li>
-                        )
-                    })}
-                </Collection.Grid>
-            </Collection.Section>
-        </>
+                    return (
+                        <li key={p.id}>
+                            <ProductProvider product={p}>
+                                <Product.Preview {...{ title, href }} />
+                                {/* <Product.Swatch attached /> */}
+                            </ProductProvider>
+                        </li>
+                    )
+                })}
+            </Collection.Grid>
+        </Collection.Section>
     )
 }
